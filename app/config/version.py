29d3babca76ff79edd3d0d8da93211d5b5c0fb24
AntonioDiogenes <antonioneto.ph3@gmail.com>
2026-08-27 12:58:05 -0300

    bump: versao 1.0.1 com auto-updater e splash screen

--- app/config/version.py
+++ app/config/version.py
@@ -1,7 +1,7 @@
 """
 Configurações Globais de Versionamento do Aplicativo DKA Alinhamento.
 """
-CURRENT_VERSION = "1.0.0"
+CURRENT_VERSION = "1.0.1"
 APP_NAME = "DKA Alinhamento"
 
 # URL de checagem do manifesto de versão mais recente
