616ce3d7cd16818f5a5329ffc8fbc1ced974364a
AntonioDiogenes <antonioneto.ph3@gmail.com>
2026-09-10 17:18:08 -0300

    config: apontar checagem de versao para a secao de Downloads do dka-desktop

--- app/config/version.py
+++ app/config/version.py
@@ -5,4 +5,4 @@ CURRENT_VERSION = "1.1.5"
 APP_NAME = "DKA Alinhamento"
 
 # URL de checagem do manifesto de versão mais recente
-VERSION_CHECK_URL = "https://bitbucket.org/cristofoleti_renato/dka-desktop/raw/main/version.json"
+VERSION_CHECK_URL = "https://bitbucket.org/cristofoleti_renato/dka-desktop/downloads/version.json"
