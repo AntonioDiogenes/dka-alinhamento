bb1655ac155396c1b532835d778ba5a4a7d58b5c
AntonioDiogenes <antonioneto.ph3@gmail.com>
2026-09-10 12:46:51 -0300

    ci: adicionar configuracao do Bitbucket Pipelines e atualizar URLs de versao

--- app/config/version.py
+++ app/config/version.py
@@ -5,4 +5,4 @@ CURRENT_VERSION = "1.1.5"
 APP_NAME = "DKA Alinhamento"
 
 # URL de checagem do manifesto de versão mais recente
-VERSION_CHECK_URL = "https://raw.githubusercontent.com/AntonioDiogenes/dka-alinhamento/main/version.json"
+VERSION_CHECK_URL = "https://bitbucket.org/cristofoleti_renato/dka-tkinter/raw/main/version.json"
